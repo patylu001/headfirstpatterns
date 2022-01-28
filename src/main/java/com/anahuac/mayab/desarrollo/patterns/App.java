49cbd7e4fc7070148844601e5f1ca6f20ad42e9b
patylu001 <patricia.ortegon@ibm.com>
2022-01-28 16:25:34 -0600

    Singleton

---
+++ src/main/java/com/anahuac/mayab/desarrollo/patterns/App.java
@@ -0,0 +1,13 @@
+package com.anahuac.mayab.desarrollo.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
